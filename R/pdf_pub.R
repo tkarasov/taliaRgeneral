e94d3ea5db923ce80c549d5835cc270eec1b37ad
tkarasov <tkarasov@gmail.com>
2019-12-12 16:31:28 +0100

    Update pdf_pub.R

--- R/pdf_pub.R
+++ R/pdf_pub.R
@@ -6,5 +6,5 @@
 
 # 88mm = 3.46in
 pdf_pub<-function(title, location = NULL, width = 3.46, height = NULL){
-  pdf(paste(location, title, sep=""), width = width, height = height, family = "Arial", useDingbats=FALSE))
+  pdf(paste(location, title, sep=""), width = width, height = height, family = "Arial", useDingbats=FALSE)
   }
