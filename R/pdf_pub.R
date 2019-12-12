46b3fee47fa181c8affff34a0253c5a85c2d5d78
tkarasov <tkarasov@gmail.com>
2019-12-12 16:28:57 +0100

    Update pdf_pub.R

--- R/pdf_pub.R
+++ R/pdf_pub.R
@@ -1,8 +1,8 @@
 # Function that will generate a pdf 
-@param title Title for the figure
-@param location Location for file to be saved
-@param width Width of figure
-@param height Height of figure
+# @param title Title for the figure
+# @param location Location for file to be saved
+# @param width Width of figure
+# @param height Height of figure
 
 
 pdf_pub<-function(title, location = NULL, width = 88mm, height = NULL){
