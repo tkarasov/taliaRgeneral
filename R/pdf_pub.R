56729c07db35d521d921dc35095bd16269617033
tkarasov <tkarasov@gmail.com>
2019-12-12 16:30:57 +0100

    Update pdf_pub.R

--- R/pdf_pub.R
+++ R/pdf_pub.R
@@ -4,7 +4,7 @@
 # @param width Width of figure
 # @param height Height of figure
 
-
-pdf_pub<-function(title, location = NULL, width = 88mm, height = NULL){
-  pdf(paste(location, title, sep=""), width = 88mm, height = height, family = "Arial", useDingbats=FALSE))
+# 88mm = 3.46in
+pdf_pub<-function(title, location = NULL, width = 3.46, height = NULL){
+  pdf(paste(location, title, sep=""), width = width, height = height, family = "Arial", useDingbats=FALSE))
   }
