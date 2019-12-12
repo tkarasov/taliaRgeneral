93822c3dd2a446410d98cf8d2ba474f40017261b
tkarasov <tkarasov@gmail.com>
2019-12-12 15:24:59 +0100

    Create pdf_pub.R

---
+++ pdf_pub.R
@@ -0,0 +1,10 @@
+# Function that will generate a pdf 
+@param title Title for the figure
+@param location Location for file to be saved
+@param width Width of figure
+@param height Height of figure
+
+
+pdf_pub<-function(title, location = NULL, width = 88mm, height = NULL){
+  pdf(paste(location, title, sep=""), width = 88mm, height = height, family = "Arial", useDingbats=FALSE))
+  }
