ed5d9cc5e76c73971a74e441aab16ff3085620ff
tkarasov <tkarasov@gmail.com>
2020-01-08 17:03:22 +0100

    Update font_pubs.R

--- R/font_pubs.R
+++ R/font_pubs.R
@@ -1,9 +1,9 @@
-theme_pubs <-
-theme(
-  text = element_text(family = "ArialMT", size = 24), 
-  plot.title = element_text(size = 32),
-  plot.caption = element_text(size = 28),
-  axis.text.x = element_text(size = 7),
-  axis.title.x = element_text(size = 12),
-  axis.text.y = element_text(size = 7),
-  axis.title.y = element_text(size = 12))
+# theme_pubs <-
+# theme(
+# text = element_text(family = "ArialMT", size = 24), 
+#  plot.title = element_text(size = 32),
+#  plot.caption = element_text(size = 28),
+#  axis.text.x = element_text(size = 7),
+#  axis.title.x = element_text(size = 12),
+#  axis.text.y = element_text(size = 7),
+#  axis.title.y = element_text(size = 12))
