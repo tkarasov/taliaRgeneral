076890182c4aff54c0a6d4dc4a6f2fadb2cdacbc
tkarasov <tkarasov@gmail.com>
2020-04-27 11:55:06 +0200

    Create addLevel.R

---
+++ R/addLevel.R
@@ -0,0 +1,9 @@
+# This function adds a level to a factor vector
+
+addLevel <- function(x, newlevel=NULL) {
+  if(is.factor(x)) {
+    if (is.na(match(newlevel, levels(x))))
+      return(factor(x, levels=c(levels(x), newlevel)))
+  }
+  return(x)
+}
